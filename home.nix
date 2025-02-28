0c70ff44b8d5af019ce42f02163cca27f501e82a
Gustlik501 <sevcnikar.gregor2@gmail.com>
2025-02-28 12:12:34 +0100

    first stable config

---
+++ home.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, ...}: {
+  home = {
+    username = "gustl";
+    homeDirectory = "/home/gustl";
+    stateVersion = "23.11"; 
+
+    packages = with pkgs; [
+      neofetch
+      htop
+      runelite
+    ]; 
+  };
+
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      rebuild = "sudo nixos-rebuild switch";
+    };
+  };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    plugins = with pkgs.vimPlugins; [
+      gruvbox-material
+      nerdtree
+    ];
+  };
+
+  programs.git = {
+   enable = true;
+   userEmail = "sevcnikar.gregor2@gmail.com";
+   userName = "Gustlik501";
+  };
+
+}
