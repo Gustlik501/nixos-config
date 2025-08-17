73d1166d4c7978e20502d063c6d257764ea91aba
Gustlik501 <sevcnikar.gregor2@gmail.com>
2025-08-17 16:37:40 +0200

    Fixed plasma darkomde

--- modules/home/plasma.nix
+++ modules/home/plasma.nix
@@ -1,13 +1,26 @@
-{ ... }:
+{ pkgs, ... }:
 let repoWp = builtins.toString (../../wallpapers);
 in {
-  xdg.configFile."kdeglobals" = {
-    text = ''
-      [General]
-      ColorScheme=BreezeDark
-    '';
-    force = true;
-  };
+   xdg.configFile."kdeglobals".text = ''
+    [General]
+    ColorScheme=BreezeDark
+    [KDE]
+    LookAndFeelPackage=org.kde.breezedark.desktop
+  '';
+
+  # ensure the tools exist
+  home.packages = [
+    pkgs.kdePackages.plasma-workspace
+    pkgs.kdePackages.breeze-gtk
+  ];
+
+  # (optional) make GTK apps dark too
+  gtk.enable = true;
+  gtk.theme.name = "Breeze-Dark";
+  xdg.configFile."gtk-3.0/settings.ini".text = ''
+    [Settings]
+    gtk-application-prefer-dark-theme=1
+  '';
 
   home.file.".local/share/wallpapers/dark.png".source = "${repoWp}/dark.png";
 
