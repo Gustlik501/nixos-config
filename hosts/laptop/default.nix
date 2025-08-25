5b3a285c537ba2a6806b83490530ca98a936d326
Gustlik501 <sevcnikar.gregor2@gmail.com>
2025-08-25 14:42:41 +0200

    Added VM to laptop

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -22,4 +22,30 @@
 
   boot.initrd.kernelModules = [ "i915" ];
 
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      ovmf.enable = true; # UEFI firmware
+      swtpm.enable = true; # vTPM for Win11
+      runAsRoot = false; # user session libvirt
+    };
+
+  };
+
+  programs.virt-manager.enable = true;
+
+  # Your user to groups
+  users.users.gustl.extraGroups = [
+    "libvirtd"
+    "kvm"
+  ];
+
+  # Handy tools/ISOs available on host
+  environment.systemPackages = with pkgs; [
+    virtiofsd # (mainly for Linux guests)
+    virtio-win # Windows VirtIO drivers ISO
+    spice-gtk # SPICE client support
+  ];
+
 }
