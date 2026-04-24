52e0431272becbe685f89e1b62a5bd95c2124bc0
Gustlik501 <sevcnikar.gregor2@gmail.com>
2026-04-24 13:22:18 +0200

    added osid to adguard

--- modules/services/adguard.nix
+++ modules/services/adguard.nix
@@ -6,6 +6,21 @@
     enable = true;
     port = 3000;
     settings = {
+      filters = [
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt";
+          name = "AdGuard DNS filter";
+          id = 1;
+        }
+        {
+          enabled = true;
+          url = "https://big.oisd.nl/";
+          name = "OISD Big";
+          id = 2;
+        }
+      ];
+
       filtering = {
         rewrites_enabled = true;
         rewrites = [
